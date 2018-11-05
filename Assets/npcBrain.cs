3f6307c83571441b06b298f603c7876194b868a3
RogueRye <31779039+RogueRye@users.noreply.github.com>
2018-11-05 15:06:57 -0500

    AI Brain Class Created

---
+++ Assets/npcBrain.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcBrain : MonoBehaviour {
+
+    Player controller;
+
+
+    private void Start()
+    {
+        controller = GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        PickAction();
+    }
+
+
+    void PickAction()
+    {
+        switch(controller.currentPhase)
+        {
+            case TurnPhase.Main:
+                break;
+            case TurnPhase.Casting:
+                break;
+            case TurnPhase.Combat:
+                break;
+            case TurnPhase.Attacking:
+                break;
+            case TurnPhase.End:
+                break;
+        }
+    }
+
+}
