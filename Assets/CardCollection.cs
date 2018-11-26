fb0f96240b6c70dbbb99a2cba19127de7a4e593b
RogueRye <31779039+RogueRye@users.noreply.github.com>
2018-11-26 17:12:00 -0500

    making stuff work for menu Scene

---
+++ Assets/CardCollection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+public class CardCollection : MonoBehaviour {
+
+    public CardHolder holderPrefab;
+
+    ScrollRect scrollRect;
+    Transform parentForCards;
+
+	// Use this for initialization
+	void Start () {
+        scrollRect = GetComponent<ScrollRect>();
+
+        parentForCards = scrollRect.content;
+
+        List<Card> collection = new List<Card>();
+        
+        var path = "Assets/Resources/Cards/";
+        DirectoryInfo dir = new DirectoryInfo(path);
+        FileInfo[] info = dir.GetFiles("*.*");
+        foreach(var f in info)
+        {
+            var newCard = Resources.Load(f.Name, typeof(Card)) as Card;
+
+            var newHolder = Instantiate(holderPrefab, parentForCards);
+
+            newHolder.thisCard = newCard;
+            newHolder.CreateCard();
+
+        }
+
+        
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/SaveLoad.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Text;
+
+public static class SaveLoad {
+
+    public static List<ProfileData> savedProfiles = new List<ProfileData>();
+
+    public static void SaveGames()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/deckProfiles.gd");
+        bf.Serialize(file, savedProfiles);
+        file.Close();
+    }
+
+    public static void LoadGames()
+    {
+        if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/deckProfiles.gd", FileMode.Open);
+            savedProfiles = (List<ProfileData>)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+
+    public static void SetCurrentProfile(ProfileData data)
+    {
+        ProfileData.currentProfile = data;
+    }
+}
+
+[System.Serializable]
+public class ProfileData
+{
+    public static ProfileData currentProfile;
+
+    public List<string> deckCardIndexes = new List<string>();
+    public List<CardData> cardCollection = new List<CardData>();
+}
+
+[System.Serializable]
+public class CardData
+{
+    public string cardName;
+    public int cardLVL;
+    public int cardAmount;
+
+    public CardData(Card card)
+    {
+        cardName = card.cardName;
+        cardLVL = 1;
+        cardAmount = 1;
+    }
+}
